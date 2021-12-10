4ab94ab69c95331ea67bea894afd8c90d593b101
Oliver Elliott <Oliver.Elliott@acronis.com>
2021-12-10 22:56:47 +0000

    day06 pt2

--- day06/main.py
+++ day06/main.py
@@ -8,7 +8,6 @@ fish = input[0].split(',')
 for i in range(0, len(fish)):
     fish[i] = int(fish[i])
 
-
 value_map = {}
 for i in range(0, 9):
     value_map[i] = 0
@@ -16,10 +15,9 @@ for i in fish:
     value_map[i] += 1
 
 count = 0
-while True:
+for i in range(0, 256):
     value_map_copy = value_map.copy()
     for key, value in value_map.items():
-
         if key == 6:
             value_map_copy[6] = value_map[7] + value_map[0]
         elif key == 8:
@@ -28,7 +26,5 @@ while True:
             value_map_copy[key] = value_map[key + 1]
     value_map = value_map_copy.copy()
     count += 1
-    if count == 256:
-        break
 
 print(sum(value_map.values()))
